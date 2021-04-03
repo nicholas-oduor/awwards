bdaeeeb6fc9e52e07496469fdbb271225a782cd1
nicholas-oduor <oduor5742@gmail.com>
2021-04-03 09:46:00 +0300

    added class ProjectsDescription and defined get,put & deletemethods

--- projects/views.py
+++ projects/views.py
@@ -54,4 +54,30 @@ class ProjectsList(APIView):
         serializers = ProjectsSerializer(all_merch, many=True)
         return Response(serializers.data)
     
+class ProjectsDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_projects(self, pk):
+        try:
+            return Projects.objects.get(pk=pk)
+        except Projects.DoesNotExist:
+            return Http404
+        
+    def get(self, request, pk, format=None):
+        project = self.get_projects(pk)
+        serializers = ProjectsSerializer(project)
+        return Response(serializers.data)
+    
+    def put(self, request, pk, format=None):
+        project = self.get_projects(pk)
+        serializers = ProjectsSerializer(project, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        project = self.get_projects(pk)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
